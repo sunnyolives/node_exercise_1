99bd03f43c376c64257e74964357e15e00761ea6
sunnyolives <alfa@krll.com>
2017-10-12 22:53:35 +0200

    Forbedringer på 1#, nå fungerer use strict

--- greeter.js
+++ greeter.js
@@ -1,26 +1,11 @@
+// KVA GJER EG: Plukker ut en tilfeldig hilsen
+// init ----------------------------
+	'use strict'
 
-  // init ----------------------------
+	var	greetingsArray = ["Ohayou gozaimasu!"];
+	greetingsArray.push("Vær hilset, fremmede!", "Velkommen til galaksen!", "God morgen!");
 
-	greetingsArray = [];
-	greetingsArray[0] = "God morgen";
-	greetingsArray[1] = "Vær hilset, fremmede";
-	greetingsArray[2] = "Velkommen til galaksen";
+	var tilfeldigIndeks = Math.floor(Math.random()*greetingsArray.length);
 
-	tilfeldigTall = Math.floor(Math.random()*3);
+	console.log(greetingsArray[tilfeldigIndeks]);
 
-	console.log(greetingsArray[tilfeldigTall]);
-
-/*
-lol, første feil "undefined": array går fra 0 til 2, ikke 1 til 3
-andre feil: math.random: det heter Math.round, ikke round. og floor, om man vil ned til 0.
-
-
-forstå:
-* Math.random bruk  https://www.freecodecamp.org/challenges/generate-random-whole-numbers-with-javascript
-* floor
-* round
-
-lær litt bedre:
-* array
-
-*/
